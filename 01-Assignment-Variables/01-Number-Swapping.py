1bde547acdc67307eb8b406103c5ac1d89fca93c
nikhil74589 <nikhilranjan2501@gmail.com>
2026-05-04 15:44:23 +0530

    python-Assignments-2026-Nikhil

---
+++ 01-Assignment-Variables/01-Number-Swapping.py
@@ -0,0 +1,12 @@
+# 1. Switch values of two integers 
+n1=20
+n2=30
+print(f"Before Swapping value of n1 : {n1}" )
+print(f"Before Swapping value of n2 : {n2}" )
+
+x=n1
+n1=n2
+n2=x
+print("-"*50)
+print(f"After Swapping value of n1 : {n1}" )
+print(f"After Swapping value of n1 : {n1}" )
---
+++ 01-Assignment-Variables/02-Update-Balance.py
@@ -0,0 +1,13 @@
+# Update balance after deposit 
+current_balance = 10000 
+deposit_balance = 5000 
+print(f"Current Balance : {current_balance}")
+print(f"Deposit Balance : {deposit_balance}")
+
+current_balance=current_balance+deposit_balance
+print(f"After deposit : {current_balance}")
+
+withdraw=200
+print(f"Withdraw amount : {withdraw}")
+current_balance=current_balance-withdraw
+print(f"After Withdraw : {current_balance}")
