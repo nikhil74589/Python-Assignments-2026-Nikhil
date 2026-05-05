83e7276b8b0dc45161c0d7e417be1855b7b12962
nikhil74589 <nikhilranjan2501@gmail.com>
2026-05-05 13:43:24 +0530

    python-Assignments-2026-Nikhil

---
+++ 01-Assignment-Variables/03-Update-Words.py
@@ -0,0 +1,13 @@
+# Switch values of two strings (characters) 
+n1="hello"
+n2="java"
+
+print(f"Before swapping of value of n1 : {n1}")
+print(f"Before swapping of value of n2 : {n2}")
+
+y=n1
+n1=n2
+n2=y
+print("-"*50)
+print(f"After Swapping value of n1 : {n1}" )
+print(f"After Swapping value of n2 : {n2}" )
