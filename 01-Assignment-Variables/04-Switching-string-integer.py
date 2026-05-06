3a5ab283f4cde519029777753eebc58a8a6229d2
nikhil74589 <nikhilranjan2501@gmail.com>
2026-05-06 11:21:17 +0530

    Some programe added

---
+++ 01-Assignment-Variables/04-Switching-string-integer.py
@@ -0,0 +1,13 @@
+# Switch one string value with one integer value
+n1=200
+chr2= "java"
+
+print(f"Before swapping of value of n1 : {n1}")
+print(f"Before swapping of value of chr2 : {chr2}")
+
+y=n1
+n1=chr2
+chr2=y
+print("-"*50)
+print(f"After swapping of value of n1 : {n1}")
+print(f"After swapping of value of chr2 : {chr2}")
